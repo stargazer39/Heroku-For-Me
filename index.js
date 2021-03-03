38b60874905e21c2d2048b458e384bfd3ca00795
stargazer39 <dehemiweerakkodi2@gmail.com>
2021-03-03 18:45:24 +0530

    http-keep-alive

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ var httpSrv = http.createServer((req,res)=>{
 	var query = URL.parse(req.url,true).query
 	res.setHeader("Access-Control-Allow-Origin",'*');
     //res.writeHead(200,{'Content-Type' : 'text/plain'});
-    res.writeHead(200,{'Content-Type': 'video/x-matroska'});
+    res.writeHead(200,{
+    	'Content-Type': 'video/x-matroska',
+    	"Connection": "keep-alive"
+    });
     const ffmpeg = spawn('ffmpeg',['-i',query.url,'-c:a','aac','-c:v','libx264','-preset','fast','-f','matroska','pipe:1']);
 	ffmpeg.stderr.on('data',(data)=>{
 		//res.write(data.toString('utf-8'));
