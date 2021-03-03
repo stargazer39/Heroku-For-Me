e1d44dc8dfd34589acb43c516ca352f4ce080e19
stargazer39 <dehemiweerakkodi2@gmail.com>
2021-03-03 21:29:02 +0530

    commit

--- index.js
+++ index.js
@@ -4,17 +4,44 @@ const URL = require('url');
 
 
 const port = process.env.PORT || 80;
+const ping_server = "ping-pong-is-awsome.herokuapp.com";
 console.log("Heroku is Working");
 
 var httpSrv = http.createServer((req,res)=>{
 	var query = URL.parse(req.url,true).query
-	res.setHeader("Access-Control-Allow-Origin",'*');
+	//res.setHeader("Access-Control-Allow-Origin",'*');
     //res.writeHead(200,{'Content-Type' : 'text/plain'});
-    res.writeHead(200,{
+    var pinger = setInterval(pingPong,1000);
+    var spawn_args = [];
+    switch(query.mode){
+    	case "sliit":
+    		spawn_args = ['-i',query.url,'-c:a','aac','-c:v','libx264','-preset','fast','-f','matroska','pipe:1'];
+    		videoStreamer(spawn_args,req,res);
+    		break;
+    	case "ytstream":
+    		spawn_args = ['-i',query.url,'-c:a','aac','-b:a','256K','-vf','scale=640x360','-c:v','libx265','-preset','fast','-f','matroska','pipe:1'];
+    		videoStreamer(spawn_args,req,res);
+    		break;
+    }
+
+    req.on("close", function() {
+	    clearInterval(pinger);
+	});
+
+	req.on("end", function() {
+	    clearInterval(pinger);
+	});
+
+})
+
+httpSrv.listen(port);
+
+function videoStreamer(spawn_args,req_,res_){
+	res.writeHead(200,{
     	'Content-Type': 'video/x-matroska',
     	"Connection": "keep-alive"
     });
-    const ffmpeg = spawn('ffmpeg',['-i',query.url,'-c:a','aac','-c:v','libx264','-preset','fast','-f','matroska','pipe:1']);
+    const ffmpeg = spawn('ffmpeg',spawn_args);
 	ffmpeg.stderr.on('data',(data)=>{
 		//res.write(data.toString('utf-8'));
 		//res.pipe
@@ -42,7 +69,28 @@ var httpSrv = http.createServer((req,res)=>{
 		res.write(`Opening FFMPEG faild ${err}`);
     	res.end();
 	})
+}
 
-})
-
-httpSrv.listen(port);
+function pingPong(){
+	var options = {
+		host:ping_server,
+		path:"/",
+		port:"80",
+		method:"GET"
+	}
+	//console.log("req");
+	var str = "";
+	var req = http.request(options,(response)=>{
+		response.on('data', function (chunk) {
+		    str += chunk;
+		});
+	    //the whole response has been received, so we just print it out here
+	    response.on('end', function () {
+	       //console.log(str);
+	    });
+	});
+	req.on('error',(err)=>{
+		console.log(`Couldn't req with error ${err}`)
+	})
+	req.end();
+}
