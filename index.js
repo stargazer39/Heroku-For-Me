873ae3ad7cc2836583386aca7a56678db6e62878
stargazer39 <dehemiweerakkodi2@gmail.com>
2021-03-03 15:21:46 +0530

    test

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Heroku is Working")
+const { spawn } = require('child_process');
+
+const ffmpeg = spawn('ls','.');
+
+ffmpeg.stderr.on('data',(data)=>{
+	console.log(data);
+})
+
+ffmpeg.on('close',(code)=>{
+	console.log(`child process exited with code ${code}`)
+})
+console.log("Heroku is Working");
+
