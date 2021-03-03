a8c6fc0beec139676cc30a9bb60f571538e60cb9
stargazer39 <dehemiweerakkodi2@gmail.com>
2021-03-03 16:52:09 +0530

    test http

--- index.js
+++ index.js
@@ -6,14 +6,19 @@ console.log("Heroku is Working");
 
 var httpSrv = http.createServer((req,res)=>{
 	res.setHeader("Access-Control-Allow-Origin",'*');
-    res.writeHead(200,{'Content-Type' : 'text/plain'});
-
-    const ffmpeg= spawn('ffmpeg',['-v']);
+    //res.writeHead(200,{'Content-Type' : 'text/plain'});
+    res.writeHead(200,{'Content-Type': 'video/mp4'});
+    const url_ = "https://lecturecapture.sliit.lk/archive/saved/Personal_Capture/2020-07-09/IT1060-Software-Process-Modeling_6935/IT1060-Software-Process-Modeling_6935_144.m3u8";
+    const file = "G:\\Data\\Media\\【 Hatsune Miku 】\\【Hatsune Miku】【初音ミク】livetune feat. Hatsune Miku「Catch the Wave」Music Video【Project DIVA MEGA39's】 ( 1080 X 1920 ).mp4";
+    const ffmpeg = spawn('ffmpeg',['-i',url_,'-c:a','aac','-c:v','libx264','-preset','slow','-f','matroska','pipe:1']);
 
 	ffmpeg.stderr.on('data',(data)=>{
-		res.write(data.toString('utf-8'));
+		//res.write(data.toString('utf-8'));
+		//res.pipe
+		console.log(data.toString('utf-8'));
 	})
 
+	ffmpeg.stdout.pipe(res);
 	ffmpeg.on('close',(code)=>{
 		console.log(`child process exited with code ${code}`);
 		res.end();
