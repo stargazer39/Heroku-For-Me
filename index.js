d77e38f56a7c0d60d90d1d2fc7cdb1a4ab140b3f
stargazer39 <dehemiweerakkodi2@gmail.com>
2021-03-05 18:33:43 +0530

    lot of changes

--- index.js
+++ index.js
@@ -2,30 +2,66 @@ const { spawn } = require('child_process');
 const http = require('http');
 const URL = require('url');
 
+class LogKeeper{
+	constructor(){
+		this.log_array = [];
+	}
+	log(tag,content,cons){
+		let str = `${new Date().toString()} ${tag} : ${content}`
+		if(this.log_array.length > 1000) this.log_array.shift();
+		this.log_array.push(str);
+		if(cons) console.log(str);
+	}
+}
+
+var headers = {
+	mkv:{
+    	'Content-Type': 'video/x-matroska',
+    	"Connection": "keep-alive"
+    	},
+    plain_text:{
+    	'Content-Type' : 'text/plain'
+    }
+}
+
+var ffmpeg_args = {
+	sliit(url){
+		return ['-i',url,'-flags','+ildct','-c:a','aac','-b:a','256K','-c:v','libx264','-preset','slow','-f','matroska','pipe:1']
+	},
+	ytstream(url){
+		return ['-i',url,'-c:a','aac','-b:a','256K','-vf','scale=640x360','-c:v','libx265','-preset','fast','-f','matroska','pipe:1']
+	}
+}
 
 const port = process.env.PORT || 80;
 const ping_server = "ping-pong-is-awsome.herokuapp.com";
 console.log("Heroku is Working");
 var isPingiging = false;
 var pinger;
+var logger = new LogKeeper();
+
 var httpSrv = http.createServer((req,res)=>{
-	var query = URL.parse(req.url,true).query
+	var query = URL.parse(req.url,true).query;
+	logger.log("MODE",query.mode,true);
 	//res.setHeader("Access-Control-Allow-Origin",'*');
     //res.writeHead(200,{'Content-Type' : 'text/plain'});
     pingpls = false;
-    var spawn_args = [];
     switch(query.mode){
     	case "sliit":
     		pingpls = true;
-    		spawn_args = ['-i',query.url,'-c:a','aac','-c:v','libx264','-preset','fast','-f','matroska','pipe:1'];
-    		videoStreamer(spawn_args,req,res);
+    		videoStreamer(ffmpeg_args.sliit(query.url),req,res);
     		break;
     	case "ytstream":
     		pingpls = true;
-    		spawn_args = ['-i',query.url,'-c:a','aac','-b:a','256K','-vf','scale=640x360','-c:v','libx265','-preset','fast','-f','matroska','pipe:1'];
-    		videoStreamer(spawn_args,req,res);
+    		videoStreamer(ffmpeg_args.ytstream(query.url),req,res);
+    		break;
+    	case "getlog":
+    		res.writeHead(200,headers.plain_text);
+    		res.write(logger.log_array.join("\n"));
+    		res.end();
     		break;
     	case "hi":
+    		res.writeHead(200,headers.plain_text);
     		res.write("Hi for the other side");
     		res.end();
     		break;
@@ -49,38 +85,48 @@ var httpSrv = http.createServer((req,res)=>{
 
 httpSrv.listen(port);
 
-function videoStreamer(spawn_args,req_,res_){
-	res_.writeHead(200,{
-    	'Content-Type': 'video/x-matroska',
-    	"Connection": "keep-alive"
-    });
+function videoStreamer(spawn_args,request,response){
     const ffmpeg = spawn('ffmpeg',spawn_args);
-	ffmpeg.stderr.on('data',(data)=>{
-		//res.write(data.toString('utf-8'));
-		//res.pipe
-		//console.log(data.toString('utf-8'));
+    var started = false;
+    var data_sent = 0;
+
+	ffmpeg.stdout.on("data",(data)=>{
+		if(!started){
+			logger.log("VStreamer",`FFMPEG Started PID ${ffmpeg.pid}`,true);
+			response.writeHead(200,headers.mkv);
+			started = true;
+		}
+		response.write(data);
+		data_sent += data.length;
 	})
 
-	ffmpeg.stdout.pipe(res_);
+	ffmpeg.on("error",(err)=>{
+		let msg = `Opening FFMPEG faild ${err}`
+		logger.log("VStreamer",msg,true);
+		response.writeHead(200,headers.text_plain);
+		response.write(msg);
+    	response.end();
+	})
 
-	req_.on("close", function() {
-	    console.log(`FFMPEG killed`);
+	ffmpeg.stderr.on('data',(data)=>{
+		//
+	})
+
+	request.on("close", function() {
+	    logger.log("VStreamer","Request closed",true);
+	    logger.log("VStreamer",`data sent ${data_sent/(1024*1024)} MB`,true);
 		ffmpeg.kill('SIGKILL');
 	});
 
-	req_.on("end", function() {
-	    console.log(`FFMPEG killed`);
+	request.on("end", function() {
+	    logger.log("VStreamer","Request ended",true);
+	    logger.log("VStreamer",`data sent ${data_sent/(1024*1024)} MB`,true);
 		ffmpeg.kill('SIGKILL');
 	});
 	
-	ffmpeg.on('close',(code)=>{
-		console.log(`child process exited with code ${code}`);
-		res_.end();
-	})
-
-	ffmpeg.on('error',(err)=>{
-		res_.write(`Opening FFMPEG faild ${err}`);
-    	res_.end();
+	ffmpeg.on("close",(code)=>{
+		logger.log("FFMPEG",`ffmpeg process exited with code ${code}`,true);
+		response.end();
 	})
 }
 
@@ -103,7 +149,7 @@ function pingPong(){
 	    });
 	});
 	req.on('error',(err)=>{
-		console.log(`Couldn't req with error ${err}`)
+		logger.log("PingPong",`Couldn't req with error ${err}`,true);
 	})
 	req.end();
 }
